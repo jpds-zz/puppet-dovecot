ac8124cb41a50ea4640cd81d17caf9175a5bbd6b
Jonathan Davies <jpdavs@gmail.com>
2015-03-08 13:57:08 +0000

    maildirmake.pp: Create mail subdirectories by default.

--- manifests/maildirmake.pp
+++ manifests/maildirmake.pp
@@ -3,7 +3,7 @@ define dovecot::maildirmake (
   $user,
 ) {
   exec { "maildirmake.dovecot-${directory}":
-    command => "maildirmake.dovecot ${directory}",
+    command => "maildirmake.dovecot ${directory} && maildirmake.dovecot ${directory}/.Drafts && maildirmake.dovecot ${directory}/.Sent && maildirmake.dovecot ${directory}/.Trash && maildirmake.dovecot ${directory}/.Templates",
     creates => "${directory}",
     path    => '/bin:/usr/bin',
     user    => "${user}",
