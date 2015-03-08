3b3d50e55a82a8c578efbfca0902404d1de2588f
Jonathan Davies <jpdavs@gmail.com>
2015-03-08 15:52:36 +0000

    maildirmake.pp: Ensure base class included before doing anything.

--- manifests/maildirmake.pp
+++ manifests/maildirmake.pp
@@ -2,6 +2,12 @@ define dovecot::maildirmake (
   $directory,
   $user,
 ) {
+  # The base class must be included first because it is used by parameter
+  # defaults.
+  if ! defined(Class['dovecot']) {
+    fail('You must include the dovecot base class before using any dovecot defined resources')
+  }
+
   exec { "maildirmake.dovecot-${directory}":
     command => "maildirmake.dovecot ${directory} && maildirmake.dovecot ${directory}/.Drafts && maildirmake.dovecot ${directory}/.Sent && maildirmake.dovecot ${directory}/.Trash && maildirmake.dovecot ${directory}/.Templates",
     creates => "${directory}",
