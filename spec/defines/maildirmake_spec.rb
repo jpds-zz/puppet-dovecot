4fcbb10e04050932194d9b08301839e88f8f2b3e
Jonathan Davies <jpdavs@gmail.com>
2015-03-08 19:29:32 +0000

    maildirmake_spec.rb: Test other OSes.

--- spec/defines/maildirmake_spec.rb
+++ spec/defines/maildirmake_spec.rb
@@ -35,4 +35,47 @@ describe 'dovecot::maildirmake', :type => :define do
        }
     end
   end
+
+  context "on a Red Hat OS" do
+    describe 'create skel Maildir' do
+      let :facts do
+        {
+          :id             => 'root',
+          :is_pe          => false,
+          :osfamily       => 'RedHat',
+          :concat_basedir => '/dne',
+          :path           => '/usr/sbin:/usr/bin:/sbin:/bin',
+        }
+      end
+
+      let(:title) { 'skel-maildir' }
+      let :params do
+        {
+          :directory => '/etc/skel/Maildir',
+          :user      => 'root',
+        }
+      end
+
+      it {
+        should contain_exec('maildirmake.dovecot-/etc/skel/Maildir').with(
+          'command' => 'maildirmake.dovecot /etc/skel/Maildir && maildirmake.dovecot /etc/skel/Maildir/.Drafts && maildirmake.dovecot /etc/skel/Maildir/.Sent && maildirmake.dovecot /etc/skel/Maildir/.Trash && maildirmake.dovecot /etc/skel/Maildir/.Templates',
+          'path'    => '/bin:/usr/bin',
+          'user'    => 'root',
+          'creates' => '/etc/skel/Maildir'
+         )
+       }
+    end
+  end
+
+  context "on an unknown OS" do
+    let :facts do
+      {
+        :osfamily => 'Darwin'
+      }
+    end
+
+    it {
+      expect { should raise_error(Puppet::Error) }
+    }
+  end
 end
