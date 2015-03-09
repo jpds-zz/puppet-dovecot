01bd4a163bd31992871cc72fa516e5005e244580
Jonathan Davies <jpdavs@gmail.com>
2015-03-09 08:36:04 +0000

    init.pp: Enable management of 10-master.conf file.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,14 @@
 # Default Dovecot class.
 class dovecot(
-  $package_core   = $dovecot::params::package_core,
-  $package_imapd  = $dovecot::params::package_imapd,
-  $package_pop3d  = $dovecot::params::package_pop3d,
-  $service_name   = $dovecot::params::service,
-  $enable_imap    = $dovecot::params::enable_imap,
-  $enable_pop3    = $dovecot::params::enable_pop3,
-  $protocols      = $dovecot::params::protocols,
-  $login_greeting = 'Dovecot ready.',
+  $package_core       = $dovecot::params::package_core,
+  $package_imapd      = $dovecot::params::package_imapd,
+  $package_pop3d      = $dovecot::params::package_pop3d,
+  $service_name       = $dovecot::params::service,
+  $enable_imap        = $dovecot::params::enable_imap,
+  $enable_pop3        = $dovecot::params::enable_pop3,
+  $protocols          = $dovecot::params::protocols,
+  $imap_process_limit = '1024',
+  $login_greeting     = 'Dovecot ready.',
 ) inherits dovecot::params {
   package { $package_core:
     ensure => installed,
@@ -57,6 +58,15 @@ class dovecot(
     mode   => '0755',
     owner  => 'root',
     group  => 'root',
+  }
+
+  file { "${dovecot::params::dovecot_conf_d}/10-master.conf":
+    ensure  => present,
+    content => template('dovecot/10-master.conf.erb'),
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    notify  => Class['Dovecot::Service'],
   }
 }
 
